d81eea6d352b325cb13deca5c7785353cad6dfac
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-04-14 21:37:09 -0600

    truck info structure made

--- src/containers/truckInfo/TruckInfo.js
+++ src/containers/truckInfo/TruckInfo.js
@@ -6,9 +6,22 @@ export default class TruckInfo extends Component{
     }
     render(){
         return(
-            <div>
-                <h1>hello world</h1>
-            </div>
+            <section className="truck-info-container">
+                <article>
+                    <div className="picture">
+                        picture of the truck
+                    </div>
+                    <div className="info">
+                        truck type like cuisine rating 
+                    </div>
+                    <div className="description">
+                        truck description
+                    </div>
+                    <div className="menu">
+                        menu
+                    </div>
+                </article>
+            </section>
         )
     }
 }
